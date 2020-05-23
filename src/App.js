8577491caff7eaf6c04319e675fac335696dfa89
Oluwafemi Babayemi <femi.gabs@gmail.com>
2020-05-23 21:57:14 +0100

    working login page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
-import Signup from './pages/signup/Signup'
+import Signup from './pages/signup/Signup';
+import Login from './pages/login/Login';
 
 function App() {
   return (
     <div className="App">
-      <Signup />
+      <Switch>
+        <Route exact path='/signup' component={Signup} />
+        <Route exact path='/login' component={Login} />
+      </Switch>
     </div>
   );
 }
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -25,11 +25,11 @@ const Signup = () => {
         //     })
     };
 
-    const { register, handleSubmit, errors } = useForm();
+    const { register, handleSubmit, errors, watch } = useForm();
 
     const togglePasswordVisiblity = () => {
         setPasswordShown(passwordShown ? false : true);
-      };
+    };
 
     return (
         <div className="signup">
@@ -39,33 +39,105 @@ const Signup = () => {
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <div className="form-row">
                         <div className="form-group col-md-6">
-                            <label className="">First Name</label>
-                            <input className="form-control"  type="text" placeholder="FirstName"  name="first_name" ref={register({ required: "LASTNAME REQUIRED", minLength: 3 })} />
+                            <label>First Name</label>
+                            <input
+                                className="form-control"
+                                type="text"
+                                placeholder="FirstName"
+                                name="first_name"
+                                ref={register({
+                                    required: "First Name Required",
+                                    minLength: {
+                                        value: 3,
+                                        message: "Too Short"
+                                    }
+                                })}
+                            />
+                            <p>{errors.first_name && errors.first_name.message}</p>
                         </div>
                         <div className="form-group col-md-6">
                             <label>Last Name</label>
-                            <input className="form-control" type="text" placeholder="LastName" name="last_name" ref={register({ required: "FIRSTNAME REQUIRED", minLength: 3 })} />
+                            <input
+                                className="form-control"
+                                type="text" 
+                                placeholder="LastName"
+                                name="last_name"
+                                ref={register({
+                                    required: "Last Name Required",
+                                    minLength: {
+                                        value: 3,
+                                        message: "Too Short"
+                                    }
+                                })}
+                            />
+                            <p>{errors.last_name && errors.last_name.message}</p>
                         </div>
                         <div className="form-group col-md-6">
                             <label>Email Address</label>
-                            <input className="form-control" type="text" placeholder="Email" name="email_address" ref={register({ required: "EMAIL REQUIRED" })} />
+                            <input
+                                className="form-control"
+                                type="text"
+                                placeholder="Email"
+                                name="email_address"
+                                ref={register({
+                                    required: "Email Required",
+                                    pattern: {
+                                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                                        message: "Invalid Email Address"
+                                    }
+                                })}
+                            />
+                            <p>{errors.email_address && errors.email_address.message}</p>
                         </div>
                         <div className="form-group col-md-6">
                             <label>Phone Number</label>
-                            <input className="form-control" type="number" placeholder="Phone Number" name="phone_number" ref={register({ required: "PHONE NUMBER REQUIRED" })} />
+                            <input
+                                className="form-control"
+                                type="number"
+                                placeholder="Phone Number"
+                                name="phone_number"
+                                ref={register({
+                                    required: "Phone Number Required"
+                                })}
+                            />
+                            <p>{errors.phone_number && errors.phone_number.message}</p>
                         </div>
-                        <div className="form-group col-md-6 has-feedback">
+                        <div className="form-group col-md-6">
                             <label>Password</label>
-                            <input className="form-control" type={passwordShown ? "text" : "password"} placeholder="Password" name="password" ref={register({ required: "PASSWORD REQUIRED", minLength: { value: 7, message: "TOO SHORT" } })} />
-                            <i onClick={togglePasswordVisiblity}>{eye}</i>
+                            <input
+                                className="form-control"
+                                type={passwordShown ? "text" : "password"}
+                                placeholder="Password"
+                                name="password"
+                                ref={register({
+                                    required: "Password Required",
+                                    minLength: {
+                                        value: 7,
+                                        message: "Too Short"
+                                    }
+                                })}
+                            />
+                            <i className="eye-icon" onClick={togglePasswordVisiblity}>{eye}</i>
+                            <p>{errors.password && errors.password.message}</p>
                         </div>
                         <div className="form-group col-md-6">
                             <label>Confirm Password</label>
-                            <input className="form-control" type="password" placeholder="Confirm Password" name="password_confirmation" ref={register({ })} />
+                            <input
+                                className="form-control"
+                                type={passwordShown ? "text" : "password"}
+                                placeholder="Confirm Password"
+                                name="password_confirmation"
+                                ref={register({
+                                    required: "Confirm Password",
+                                    validate: (value) => value === watch('password') || "Password does not not match"
+                                })}
+                            />
+                            <i className="eye-icon" onClick={togglePasswordVisiblity}>{eye}</i>
+                            <p>{errors.password_confirmation && errors.password_confirmation.message}</p>
                         </div>
                         <div className="col-md-6 col-md-offset-3">
                             <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
-                            <span>Already have an account? <Link to='/' className="link">Sign in</Link></span>
+                            <span>Already have an account? <Link to='/login' className="link">Sign in</Link></span>
                         </div>
                     </div>
                 </form>
