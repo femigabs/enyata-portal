fec2d466f205c737c29a23db3bb9f76ac62509f3
Oluwafemi  Babayemi <oluwafemibabayemi@MacBook-Pro.local>
2020-06-24 15:13:18 +0100

    updated code

--- src/pages/error/Error.js
+++ src/pages/error/Error.js
@@ -4,10 +4,13 @@ import { NavLink } from 'react-router-dom'
 
 const Error_page = () => {
     return (
+        <>
         <h4 className="error_page">
             <span style={{'color':'red'}}>Error: 404</span>  page not found
             {/* <NavLink  className="error" to="/dashboard">Dashboard</NavLink> */}
         </h4>
+        <NavLink className="btn btn-success" exact to="/login">Login</NavLink>
+        </>
     )
 }
 
